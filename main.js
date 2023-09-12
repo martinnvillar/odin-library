331fb697cf6b9c0c74790deb061124f10cca6384
martinnvillar <martinnnvillar@icloud.com>
2023-09-12 12:37:40 -0300

    css update

--- main.js
+++ main.js
@@ -1,2 +1,23 @@
 const addBtn = document.querySelector('.addBtn');
+const cancelBtn = document.querySelector('.cancel');
+let myLibrary = [];
+
+addBtn.addEventListener('click', function() {
+    let newBook = document.querySelector('#new-book')
+    newBook.style.display = 'block';
+});
+
+cancelBtn.addEventListener('click', function(){
+    let cancelBook = document.querySelector('#new-book')
+    newBook.style.display = 'none';
+});
+
+function Book(title, author, pages, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+};
+
+
 
