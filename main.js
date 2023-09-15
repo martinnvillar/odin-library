4d1dec0f37f3d983c0b479ef0bc45334b7f569c6
martinnvillar <martinnnvillar@icloud.com>
2023-09-14 21:57:05 -0300

    css and js upd

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 const addBtn = document.querySelector('.addBtn');
 const cancelBtn = document.querySelector('.cancel');
+const bookDialog = document.querySelector('#book-dialog');
+const addTitle = document.querySelector('#title');
+const addAuthor = document.querySelector('#author');
+const numbOfPages = document.querySelector('#pages');
+const readBox = document.querySelector('#read');
+const notReadBox = document.querySelector('#not-read');
+ 
+
+addBtn.addEventListener('click', () => {
+    bookDialog.showModal();
+    document.body.classList.add('locked-scroll');
+});
+
 let myLibrary = [];
 
-addBtn.addEventListener('click', function() {
-    let newBook = document.querySelector('#new-book')
-    newBook.style.display = 'block';
-});
-
-cancelBtn.addEventListener('click', function(){
-    let cancelBook = document.querySelector('#new-book')
-    newBook.style.display = 'none';
-});
-
-function Book(title, author, pages, isRead) {
+function Book (title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
 };
+
+
 
 
 
