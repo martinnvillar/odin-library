da2aed96aefdea94aae0ab71bfeef499de9767cc
martinnvillar <martinnnvillar@icloud.com>
2023-09-25 23:55:18 -0300

    add JS update, now the cards appear when you confirm your book. need to work on some other functionialities

--- main.js
+++ main.js
@@ -2,12 +2,18 @@ const addBtn = document.querySelector('.addBtn');
 const cancelBtn = document.querySelector('.cancel');
 const bookDialog = document.querySelector('#book-dialog');
 const confirmBook = document.querySelector('.confirm');
+const cardsCont = document.querySelector('.cards-container');
 let myLibrary = [];
  
 
 addBtn.addEventListener('click', () => {
     bookDialog.showModal();
-    document.body.classList.add('locked-scroll');
+});
+
+confirmBook.addEventListener('click', () => {
+    addBookToLibrary();
+    insertBook();
+    console.log()
 });
 
 cancelBtn.addEventListener('click', (event) => {
@@ -15,21 +21,21 @@ cancelBtn.addEventListener('click', (event) => {
     bookDialog.close();
 })
 
-function Book(title, author, pages, isRead) {
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.isRead = isRead;
+    this.status = status;
 };
 
 function addBookToLibrary() {
     const addTitle = document.querySelector('#title').value;
     const addAuthor = document.querySelector('#author').value;
     const numbOfPages = document.querySelector('#pages').value;
-    const readBox = document.querySelector('#read').checked;
+    const bookStatus = document.querySelector('#read').checked;
     const bookForm = document.querySelector('form');
 
-    const book = new Book(addTitle, addAuthor, numbOfPages, readBox);
+    const book = new Book(addTitle, addAuthor, numbOfPages, bookStatus);
 
     if (addTitle !== '' & addAuthor !== '' & numbOfPages !== '') {
         myLibrary.push(book);
@@ -38,6 +44,28 @@ function addBookToLibrary() {
     };
     bookForm.reset();
 }
+
+
+function insertBook() {
+    let bookCard = '';
+    if (myLibrary) {
+        myLibrary.forEach((book, index) =>
+        bookCard += 
+        `<div class="cards-container">
+            <div class="book-card">
+                <h2>${book.title}</h2>
+                <h3>by ${book.author}</h3>
+                <p>${book.pages} pages</p>
+                <div class="card-btn">
+                    <button class="read"><p></p></button>
+                    <button class="delete-card"><img class="trash-ico" src="trash.svg" alt="trash-ico"></button>
+                </div>
+            </div>
+        </div>`
+        
+        )};
+        cardsCont.innerHTML = bookCard;
+};
 
 
 
